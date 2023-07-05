10f6a540c82bfbd0e58f03e9a2db4b6aae3fe2b6
Douglas Fugazi <itmteleco@gmail.com>
2023-07-05 17:37:42 -0500

    Prettier format code

--- cypress/e2e/lighthouse.cy.js
+++ cypress/e2e/lighthouse.cy.js
@@ -1,100 +1,100 @@
 /// <reference types="Cypress" />
 
-describe('Lighthouse Performance Test', () => {
+describe("Lighthouse Performance Test", () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit("/");
   });
 
-  it('Home Page Performance Audit', () => {
-    cy.lighthouse();
-  });
-  
-  it('Desktop - About Page Performance Audit', () => {
-    cy.visit('/info');
+  it("Home Page Performance Audit", () => {
     cy.lighthouse();
   });
 
-  it('Mobile - About Page Performance Audit', () => {
-    cy.visit('/info');
-    cy.viewport('iphone-xr', 'landscape');
+  it("Desktop - About Page Performance Audit", () => {
+    cy.visit("/info");
     cy.lighthouse();
   });
 
-  it('Mobile - Home Page Performance Audit', () => {
-    cy.viewport('iphone-6');
+  it("Mobile - About Page Performance Audit", () => {
+    cy.visit("/info");
+    cy.viewport("iphone-xr", "landscape");
     cy.lighthouse();
   });
 
-  it('MacBook - Home Page Performance Audit', () => {
-    cy.viewport('macbook-16');
+  it("Mobile - Home Page Performance Audit", () => {
+    cy.viewport("iphone-6");
     cy.lighthouse();
   });
 
-  it('iPad - Home Page Performance Audit', () => {
-    cy.viewport('ipad-mini');
+  it("MacBook - Home Page Performance Audit", () => {
+    cy.viewport("macbook-16");
     cy.lighthouse();
   });
 
-  it('Samsung - Home Page Performance Audit', () => {
-    cy.viewport('samsung-s10');
+  it("iPad - Home Page Performance Audit", () => {
+    cy.viewport("ipad-mini");
     cy.lighthouse();
   });
 
-  it('Landscape - Home Page Performance Audit', () => {
-    cy.viewport('iphone-xr', 'landscape');
+  it("Samsung - Home Page Performance Audit", () => {
+    cy.viewport("samsung-s10");
     cy.lighthouse();
   });
 
-  it('Lighthouse Performance audits using custom thresholds', () => {
+  it("Landscape - Home Page Performance Audit", () => {
+    cy.viewport("iphone-xr", "landscape");
+    cy.lighthouse();
+  });
+
+  it("Lighthouse Performance audits using custom thresholds", () => {
     const customThresholds = {
       performance: 90,
       accessibility: 90,
       seo: 85,
-      'first-contentful-paint': 2000,
-      'largest-contentful-paint': 3000,
-      'cumulative-layout-shift': 0.1,
-      'total-blocking-time': 500,
+      "first-contentful-paint": 2000,
+      "largest-contentful-paint": 3000,
+      "cumulative-layout-shift": 0.1,
+      "total-blocking-time": 500,
     };
 
     const desktopConfig = {
-      formFactor: 'desktop',
+      formFactor: "desktop",
       screenEmulation: { disabled: true },
     };
     cy.lighthouse(customThresholds, desktopConfig);
   });
 
-  it('Lighthouse Performance audits using custom values', () => {
+  it("Lighthouse Performance audits using custom values", () => {
     cy.lighthouse({
       performance: 100,
       accessibility: 100,
       seo: 100,
-      'first-contentful-paint': 2000,
-      'largest-contentful-paint': 3000,
-      'cumulative-layout-shift': 0.1,
-      'total-blocking-time': 500,
+      "first-contentful-paint": 2000,
+      "largest-contentful-paint": 3000,
+      "cumulative-layout-shift": 0.1,
+      "total-blocking-time": 500,
     });
   });
 
-  it('Lighthouse Performance audits using custom values', () => {
+  it("Lighthouse Performance audits using custom values", () => {
     cy.lighthouse({
-  chromeFlags: ['--headless', '--disable-gpu'],
-  onlyCategories: ['performance'],
-  emulatedFormFactor: 'desktop',
-  throttling: {
-    rttMs: 40,
-    throughputKbps: 10 * 1024,
-    cpuSlowdownMultiplier: 1,
-    requestLatencyMs: 0, // 0 means unset
-    downloadThroughputKbps: 0,
-    uploadThroughputKbps: 0,
-  },
-  screenEmulation: {
-    mobile: false,
-    width: 1350,
-    height: 940,
-    deviceScaleFactor: 1,
-    disabled: false,
-  },
-});
-});
+      chromeFlags: ["--headless", "--disable-gpu"],
+      onlyCategories: ["performance"],
+      emulatedFormFactor: "desktop",
+      throttling: {
+        rttMs: 40,
+        throughputKbps: 10 * 1024,
+        cpuSlowdownMultiplier: 1,
+        requestLatencyMs: 0, // 0 means unset
+        downloadThroughputKbps: 0,
+        uploadThroughputKbps: 0,
+      },
+      screenEmulation: {
+        mobile: false,
+        width: 1350,
+        height: 940,
+        deviceScaleFactor: 1,
+        disabled: false,
+      },
+    });
+  });
 });
