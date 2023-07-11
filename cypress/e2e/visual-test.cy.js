b7e931dec86bb4126924ad7c4e2bcdfcadf1c17c
Douglas Fugazi <itmteleco@gmail.com>
2023-07-11 17:15:24 -0500

    Updating Visual Tests

--- cypress/e2e/visual-test.cy.js
+++ cypress/e2e/visual-test.cy.js
@@ -1,7 +1,15 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+describe('Visit Democart main page to validate Visual Testing', () => {
+  it('Cypress-axe should log accessibility failures', () => {
+      cy.visit('https://demo.opencart.com');
+      cy.injectAxe();
+      cy.checkA11y();
   });
+
+  it('Visual Testing should exclude specific elements on the page', () => {
+      cy.visit('https://demo.opencart.com');
+      cy.injectAxe();
+      cy.checkA11y({exclude: ['#content']});
+  })
 });
