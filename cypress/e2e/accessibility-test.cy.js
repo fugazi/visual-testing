535b8f5db34f5fb71a9b0eb75d13e4a554e1d788
Douglas Fugazi <itmteleco@gmail.com>
2023-07-16 19:33:13 -0500

    Splitting tests

---
+++ cypress/e2e/accessibility-test.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Visit Democart main page to validate Visual Testing ', () => {
+  it('Cypress-axe should log accessibility failures', () => {
+      cy.visit('https://demo.opencart.com');
+      cy.injectAxe();
+      cy.checkA11y();
+  });
+
+  it('Visual Testing should exclude specific elements on the page ', () => {
+      cy.visit('https://demo.opencart.com');
+      cy.injectAxe();
+      cy.checkA11y({exclude: ['#content']});
+  })
+});
