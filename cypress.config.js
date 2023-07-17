22e6ac356b97666d3f18f205a88d830c1ef360a1
Douglas Fugazi <itmteleco@gmail.com>
2023-07-16 19:41:48 -0500

    Updating cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse");
+const getCompareSnapshotsPlugin = require('cypress-image-diff-js/dist/plugin');
 
-module.exports = {
+module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.monofonicos.net", // this is the URL your server runs on during tests
     setupNodeEvents(on, config) {
+      getCompareSnapshotsPlugin(on, config);
       on("before:browser:launch", (browser = {}, launchOptions) => {
         prepareAudit(launchOptions);
       });
@@ -14,4 +16,4 @@ module.exports = {
       });
     },
   },
-};
+});
