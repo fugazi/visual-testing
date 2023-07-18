7fdb3a5ee1c1f23e930716f7f5b0e8e3e3d17448
Douglas Fugazi <itmteleco@gmail.com>
2023-07-18 09:35:40 -0500

    Perform Visual Regression Test to validate Visual Testing

--- cypress/e2e/visual-test.cy.js
+++ cypress/e2e/visual-test.cy.js
@@ -1,15 +1,17 @@
 /// <reference types="cypress" />
 
-describe('Visit Democart main page to validate Visual Testing ', () => {
-  it('Cypress-axe should log accessibility failures', () => {
-      cy.visit('https://demo.opencart.com');
-      cy.injectAxe();
-      cy.checkA11y();
-  });
+import { addMatchImageSnapshotCommand } from "cypress-image-diff-js";
 
-  it('Visual Testing should exclude specific elements on the page ', () => {
-      cy.visit('https://demo.opencart.com');
-      cy.injectAxe();
-      cy.checkA11y({exclude: ['#content']});
-  })
+addMatchImageSnapshotCommand({
+  failureThreshold: 0.03, // threshold for image comparison
+  failureThresholdType: "percent", // type of threshold
+  customDiffConfig: { threshold: 0.1 }, // threshold for pixel diff
+  capture: "viewport", // capture the entire viewport
+});
+
+describe("Perform Visual Regression Test to validate Visual Testing", () => {
+  it("Open DemoCart page should match the snapshot", () => {
+    cy.visit("https://demo.opencart.com");
+    cy.matchImageSnapshot();
+  });
 });
