7bf12d405bdec6f8843fe3381caf9b2e49163c41
Douglas Fugazi <itmteleco@gmail.com>
2023-08-13 16:56:45 -0500

    Updating cypress tests

--- cypress/e2e/visual-test.cy.js
+++ cypress/e2e/visual-test.cy.js
@@ -4,7 +4,7 @@ import { addMatchImageSnapshotCommand } from "cypress-image-diff-js";
 
 addMatchImageSnapshotCommand({
   failureThreshold: 0.03, // threshold for image comparison
-  failureThresholdType: "percent", // type of threshold
+  failureThresholdType: "percent", // type of thresholds
   customDiffConfig: { threshold: 0.1 }, // threshold for pixel diff
   capture: "viewport", // capture the entire viewport
 });
