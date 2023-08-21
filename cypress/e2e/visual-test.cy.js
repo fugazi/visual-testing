10c4dbe44dc623a313abdb942bc8fec900f6d35a
Douglas Fugazi <itmteleco@gmail.com>
2023-08-21 13:29:05 -0500

    updating visual tests

--- cypress/e2e/visual-test.cy.js
+++ cypress/e2e/visual-test.cy.js
@@ -6,7 +6,7 @@ addMatchImageSnapshotCommand({
   failureThreshold: 0.03, // threshold for image comparison
   failureThresholdType: "percent", // type of thresholds
   customDiffConfig: { threshold: 0.1 }, // threshold for pixel diff
-  capture: "viewport", // capture the entire viewport
+  capture: "viewport", // capture the entire viewports
 });
 
 describe("Performing Visual Regression Test to validate Visual Testing", () => {
