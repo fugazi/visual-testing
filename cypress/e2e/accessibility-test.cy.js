fd598096778da18cb2b1834226c8be8f11e2c693
Douglas Fugazi <itmteleco@gmail.com>
2023-09-24 19:53:30 -0500

    Updating docstring

--- cypress/e2e/accessibility-test.cy.js
+++ cypress/e2e/accessibility-test.cy.js
@@ -1,15 +1,18 @@
 /// <reference types="cypress" />
 
-describe('Visit Democart main page to validate Visual Testing ', () => {
-  it('Cypress-axe should log accessibility failures', () => {
-      cy.visit('https://demo.opencart.com');
-      cy.injectAxe();
-      cy.checkA11y();
+/**
+ * Visits the main page of a demo e-commerce website and performs visual testing and accessibility checks using Cypress and Cypress-axe.
+ */
+describe("Visit Democart main page to validate Visual Testing ", () => {
+  it("Cypress-axe should log accessibility failures", () => {
+    cy.visit("https://demo.opencart.com");
+    cy.injectAxe();
+    cy.checkA11y();
   });
 
-  it('Visual Testing should exclude specific elements on the page ', () => {
-      cy.visit('https://demo.opencart.com');
-      cy.injectAxe();
-      cy.checkA11y({exclude: ['#content']});
-  })
+  it("Visual Testing should exclude specific elements on the page ", () => {
+    cy.visit("https://demo.opencart.com");
+    cy.injectAxe();
+    cy.checkA11y({ exclude: ["#content"] });
+  });
 });
