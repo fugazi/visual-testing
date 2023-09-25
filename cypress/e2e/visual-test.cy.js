5caf652511a980b9aaf93d1afcb072b3009e6956
Douglas Fugazi <itmteleco@gmail.com>
2023-09-24 20:00:14 -0500

    Updating docstrings for visual testing

--- cypress/e2e/visual-test.cy.js
+++ cypress/e2e/visual-test.cy.js
@@ -2,11 +2,22 @@
 
 import { addMatchImageSnapshotCommand } from "cypress-image-diff-js";
 
+/**
+ * Adds a custom command to Cypress called `addMatchImageSnapshotCommand`.
+ * This command is used for performing visual regression testing by comparing screenshots of a webpage with a reference image.
+ *
+ * @param {Object} options - The configuration options for visual regression testing.
+ * @param {number} options.failureThreshold - The threshold for image comparison, specifying the maximum percentage difference allowed between the reference image and the captured screenshot.
+ * @param {string} options.failureThresholdType - The type of threshold used for image comparison.
+ * @param {Object} options.customDiffConfig - An object containing configuration options for pixel diff.
+ * @param {number} options.customDiffConfig.threshold - The maximum percentage difference allowed between pixels.
+ * @param {string} options.capture - The area of the webpage to capture for comparison.
+ */
 addMatchImageSnapshotCommand({
-  failureThreshold: 0.03, // threshold for image comparison
-  failureThresholdType: "percent", // type of thresholds
-  customDiffConfig: { threshold: 0.1 }, // threshold for pixel diff
-  capture: "viewport", // capture the entire viewport
+  failureThreshold: 0.03,
+  failureThresholdType: "percent",
+  customDiffConfig: { threshold: 0.1 },
+  capture: "viewport",
 });
 
 describe("Performing Visual Regression Test to validate Visual Testing", () => {
