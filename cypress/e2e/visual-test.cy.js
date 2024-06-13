0463d98423bc86dda65a5b9ee6ff5fc41447685c
douglas.fugazi <douglas.fugazi@gmail.com>
2024-06-13 18:21:16 +0000

    Adding new test

--- cypress/e2e/visual-test.cy.js
+++ cypress/e2e/visual-test.cy.js
@@ -25,4 +25,18 @@ describe("Performing Visual Regression Test to validate Visual Testing", () => {
     cy.visit("https://www.monofonicos.net");
     cy.matchImageSnapshot();
   });
+
+  it("Header and Banner should match the snapshot", () => {
+    cy.visit("https://www.monofonicos.net");
+
+    // Option 1: Capture only the header and banner elements
+    cy.get("header").matchImageSnapshot("header");
+    cy.get(".main-banner").matchImageSnapshot("mainBanner");
+
+    // Option 2: Capture a specific area of the page containing header and banner
+    cy.get("body").then(($body) => {
+      const headerAndBanner = $body.find("header, .main-banner");
+      cy.wrap(headerAndBanner).matchImageSnapshot("headerAndBanner");
+    });
+  });
 });
