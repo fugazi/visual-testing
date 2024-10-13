2e79ac34878b8c5a2cc97afbfe4900c8cd46608d
Douglas Fugazi <itmteleco@gmail.com>
2024-10-13 15:48:29 -0500

    Updating image-diff-js-plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require("cypress");
 const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse");
-const getCompareSnapshotsPlugin = require('cypress-image-diff-js/dist/plugin');
+const getCompareSnapshotsPlugin = require("cypress-image-diff-js/plugin");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.monofonicos.net", // this is the URL your server runs on during tests
+    baseUrl: "https://www.monofonicos.net",
     setupNodeEvents(on, config) {
+      // Call the plugin function with on and config
       getCompareSnapshotsPlugin(on, config);
+
       on("before:browser:launch", (browser = {}, launchOptions) => {
         prepareAudit(launchOptions);
       });
@@ -14,6 +16,8 @@ module.exports = defineConfig({
       on("task", {
         lighthouse: lighthouse(),
       });
+
+      return config;
     },
   },
 });
